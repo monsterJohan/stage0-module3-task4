0331c70e57947249fed68545afc20a37fa4e17d5
monsterJohan <nikfil97@gmail.com>
2022-08-31 12:29:51 +0200

    Implemented Task4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,13 +2,5 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-
-<<<<<<<<< Temporary merge branch 1
-        System.out.println((9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b)) / 2);
-=========
-        System.out.println((9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2));
->>>>>>>>> Temporary merge branch 2
     }
 }
